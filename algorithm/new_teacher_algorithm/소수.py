a4cd8eddb0f10f8adf9230b91b1d077a6e1202a2
websvey1 <websvey1@gmail.com>
2019-03-05 23:22:36 +0900

    algo

--- algorithm/new_teacher_algorithm/소수.py
+++ algorithm/new_teacher_algorithm/소수.py
@@ -1,7 +1,53 @@
 import sys
 sys.stdin = open("소수.txt")
 
-a,b = map(int, input().split())
-# print(a)
-for i in range(a,b+1):
-    if i % 2 == 0 or i %3 == 0 or i % 5 == 0:
+a,b = list(map(int, input().split()))
+data = [i for i in range(a,b+1)]
+
+# 상근이형 소수
+def sosu(a,b):
+    sosu = list(range(b+1))
+    root = int(b**0.5)
+    sosu[1] = 0
+    # print(sosu)
+    for i in range(2, root+1):
+        # if i in sosu:
+        if sosu[i]:
+            m = i+i
+            while m < b+1:
+                sosu[m] = 0
+                m += i
+    sosu = sosu[a::]
+    sosu = [i for i in sosu if i]
+    return sosu
+result = sosu(a,b)
+
+A = result
+print(len(A))
+print(A[0]+A[-1])
+
+
+
+
+
+
+
+#인터넷 코드
+
+# import sys
+# sys.stdin = open("소수.txt")
+# 
+# L,M = list(map(int, input().split()))
+# data = [i for i in range(L,M+1)]
+# 
+# a = [False,False] + [True]*(M-1)
+# primes=[]
+# 
+# for i in range(L,M+1):
+#   if a[i]:
+#     primes.append(i)
+#     for j in range(L*i, M+1, i):
+#         a[j] = False
+# print(primes)
+# print(len(set(primes)))
+# print(min(primes)+max(primes))
---
+++ algorithm/new_teacher_algorithm/자리배정.py
@@ -0,0 +1,65 @@
+import sys
+sys.stdin = open("자리배정.txt")
+
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+def solution(C,R,target):
+    data = [[0 for _ in range(C)] for _ in range(R)]
+    cnt = 0
+    i = j = 0
+    flag = 0 # 방향전환변수 상하/좌우
+    while cnt <= C*R:
+        cnt +=1
+        if cnt == target:
+            print(data)
+            return j+1, i+1
+        data[i][j] = cnt
+        if flag %2 == 0:
+            i += dy[flag]
+            if i + dy[flag] < 0 or i + dy[flag] >=R or data[i+dy[flag]][j]:
+                flag = (flag +1) %4
+        elif flag %2 ==1:
+            j +=dx[flag]
+            if j + dx[flag] < 0 or j+dx[flag] >= C or data[i][j+dx[flag]]:
+                flag = (flag + 1) %4
+    return 0
+
+
+C,R = map(int, input().split())
+target = int(input())
+answer = solution(C,R,target)
+
+if answer:
+    print(*answer)
+else:
+    print(0)
+
+
+
+
+
+
+# for i in range(len(data)):
+#     for j in range(len(data[i])):
+#         if data[i][j] == 0:
+#             data[i][j] = cnt
+#             cnt +=1
+#     if j == len(data[i])-1:
+#         for i in range(len(data)):
+#             if data[i][j] ==0:
+#                 data[i][j] = cnt
+#                 cnt +=1
+#         if i == len(data)-1:
+#             for j in range(len(data[i])):
+#                 if data[i][R-2-j] == 0:
+#                     data[i][R-2-j] = cnt
+#                     cnt += 1
+#             if j == len(data[i])-1:
+#                 # print(j)
+#                 for i in range(len(data)):
+#                     if data[C-2-i][0] == 0:
+#                         data[C-2-i][0] = cnt
+#                         cnt +=1
+# print(data)
