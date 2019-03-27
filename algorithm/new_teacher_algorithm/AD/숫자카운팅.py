0db5b40824d2ef1e5862651ef7b4c6b7d45178eb
websvey1 <websvey1@gmail.com>
2019-03-28 08:33:06 +0900

    asd

--- algorithm/new_teacher_algorithm/AD/숫자카운팅.py
+++ algorithm/new_teacher_algorithm/AD/숫자카운팅.py
@@ -1,40 +1,77 @@
 import sys
 sys.stdin = open("숫자카운팅.txt")
-
-def lowerSerch(s,e,f):
+def lowSearch(s,e,fdata):
     sol = -1
-    while s <=e:
+    while s<=e:
         m = (s+e)//2
-        if f == data[m]:
+        if fdata == data[m]:
             sol = m
             e = m-1
-        elif f >data[m]:
-            s = m + 1
-        elif f < data[m]:
+        elif fdata > data[m]:
+            s = m+1
+        elif fdata < data[m]:
             e = m-1
     return sol
-
-def upperSerch(s,e,f):
+def upperSearch(s,e,fdata):
     sol = -1
-    while s <=e:
-        m = (s + e) // 2
-        if f == data[m]:
+    while s<=e:
+        m = (s+e)//2
+        if fdata == data[m]:
             sol = m
             s = m+1
-        elif f > data[m]:
-            s = m + 1
-        elif f < data[m]:
+        elif fdata > data[m]:
+            s = m+1
+        elif fdata < data[m]:
             e = m-1
     return sol
+
 N = int(input())
 data = list(map(int, input().split()))
 fN = int(input())
 fdata = list(map(int, input().split()))
 
 for i in range(fN):
-    lo = lowerSerch(0,fN-1,fdata[i])
-    if lo >= 0 :
-        up = upperSerch(0, N-1, fdata[i])
-        print(up-lo + 1, end=" ")
+    lo = lowSearch(0,N-1,fdata[i])
+    if lo >= 0:
+        up = upperSearch(0, N-1, fdata[i])
+        print(up-lo+1, end=" ")
     else:
         print(0, end=" ")
+
+
+
+
+#
+# def lowerSerch(s,e,f):
+#     sol = -1
+#     while s <=e:
+#         m = (s+e)//2
+#         if f == data[m]:
+#             sol = m
+#             e = m-1
+#         elif f >data[m]:
+#             s = m + 1
+#         elif f < data[m]:
+#             e = m-1
+#     return sol
+#
+# def upperSerch(s,e,f):
+#     sol = -1
+#     while s <=e:
+#
+#         m = (s + e) // 2
+#         if f == data[m]:
+#             sol = m
+#             s = m+1
+#         elif f > data[m]:
+#             s = m + 1
+#         elif f < data[m]:
+#             e = m-1
+#     return sol
+# for i in range(fN):
+#     lo = lowerSerch(0,N-1,fdata[i])
+#     if lo >= 0 :
+#         up = upperSerch(0, N-1, fdata[i])
+#         print(up-lo + 1, end=" ")
+#     else:
+#         print(0, end=" ")
---
+++ algorithm/new_teacher_algorithm/AD/저글링.py
@@ -0,0 +1,41 @@
+import sys
+sys.stdin = open('저글링.txt')
+def BFS():
+    global cnt
+    que = []
+    dy = (-1,1,0,0)
+    dx = (0,0,-1,1)
+    que.append((ir, ic, cnt))
+    data[ir][ic] = 0
+    while que:
+        r,c,cnt = que.pop(0)
+        for i in range(4):
+            nr = r+dx[i]
+            nc = c+dy[i]
+            if nr<0 or nc<0 or nr>R or nc >C: continue
+            if data[nr][nc] != 1: continue
+            que.append((nr,nc,cnt+1))
+            data[nr][nc] = 0
+
+    return cnt
+
+
+C,R = map(int, input().split())
+data = [list(map(int, input()))for _ in range(R)]
+
+ic, ir = map(int, input().split())
+CNT = 0
+cnt = 0
+# 벽씌욱
+for i in range(len(data)):
+    data[i].append(0)
+    data[i].insert(0, 0)
+data.append([0]*(C+2))
+data.insert(0, [0]*(C+2))
+a = BFS()
+for i in range(R):
+    for j in range(C):
+        if data[i][j] == 1:
+            CNT +=1
+print(a+3)
+print(CNT)
