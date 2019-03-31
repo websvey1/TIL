e5cff0876b7317daf2d1a6b902822a23f873c355
websvey1 <websvey1@gmail.com>
2019-04-01 08:51:29 +0900

    asd

--- algorithm/new_teacher_algorithm/AD/p2.py
+++ algorithm/new_teacher_algorithm/AD/p2.py
@@ -6,8 +6,21 @@ for tc in range(1,T+1):
     R,C = map(int, input().split())
     data = [list(map(int, input().split()))for _ in range(R)]
     # print(R,C,data)
-    for i in range(R-1):
-        for j in range(i+1,R):
-            for ci in range(C-2):
-                for cj in range(ci+1,C-1):
-                    for ck in range(cj+1,C):
+
+    for sr in range(R-1):
+        hap1 = 0
+        for sc in range(C-2):
+            hap1 += data[sr][sc]
+            hap3 = 0
+            for mc in range(sc+1,C-1):
+                hap3 +=data[sr][mc]
+                hap5 = 0
+                hap6 = 0
+                hap4 = 0
+                hap2 = 0
+                for er in range(sr + 1, R):
+                    hap4 += data[er][mc]
+                    hap2 += data[er][sc]
+                    for ec in range(mc + 1, C):
+                        hap6 += data[er][ec]
+                        hap5 += data[sr][ec]
--- algorithm/new_teacher_algorithm/AD/test_p2.py
+++ algorithm/new_teacher_algorithm/AD/test_p2.py
@@ -1,25 +1,41 @@
 import sys
 sys.stdin = open('test_p2.txt')
+def dfs(n,cnt):
+    global result
+    if n >=len(comp):
+        print(result)
+        return
+    result[n] = comp[n]
+    dfs(n+1, cnt+1)
+    result[n] = 0
+    dfs(n+1, cnt)
+
 
 T = int(input())
 for tc in range(1,T+1):
     R,C = map(int, input().split())
     data = [list(map(int, input().split()))for _ in range(R)]
-    a,b,c,d = 0,0,0,0
-    for i in range(R):
-        for j in range(C):
-            hap1 = 0
-            hap2 = 0
-            for si in range(i,R-1):
-                for sj in range(j, C - 1):
-                    for ei in range(si+1,R):
-                        hap1 += data[si][sj]
-                        hap2 += data[ei][sj]
-                        hap3,hap4 = 0,0
-                        for ej in range(sj+1,C):
-                            hap3+=data[si][ej]
-                            hap4+=data[ei][ej]
-                    print(hap1, end=" ")
-                    print(hap2, end=" ")
-                    print(hap3, end=" ")
-                    print(hap4)
+    cnt = 0
+    hap1 = 0
+    hap2 = 0
+    result = [0 for _ in range(10)]
+    for si in range(R-1):
+        for sj in range(C - 1):
+            hap3, hap4 = 0, 0
+            comp = []
+            for ei in range(si+1,R):
+                hap1 += data[si][sj]
+                hap2 += data[ei][sj]
+                for ej in range(sj+1,C):
+                    hap3 += data[si][ej]
+                    hap4 += data[ei][ej]
+                comp.append(hap3)
+                comp.append(hap4)
+            comp.append(hap1)
+            comp.append(hap2)
+            # print(hap1, end=" ")
+            # print(hap2, end=" ")
+            # print(hap3, end=" ")
+            # print(hap4)
+            print(comp)
+            dfs(0,cnt)
