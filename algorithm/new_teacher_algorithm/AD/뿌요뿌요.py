d4ad67dc3caf8b24251840ab74c541d04ac92704
websvey1 <websvey1@gmail.com>
2019-04-05 22:11:55 +0900

    BBUYOBBUYO

--- algorithm/new_teacher_algorithm/AD/뿌요뿌요.py
+++ algorithm/new_teacher_algorithm/AD/뿌요뿌요.py
@@ -12,17 +12,25 @@ def check(x,y):
     return RR,GG,BB,YY,PP
 def clean():
     # recover = [[0 for _ in range(12)]for _ in range(6)]
-    recover = []
-    for i in range(6):
-        for j in range(12):
-            if re_data[i][j] != '.':
-                recover.append(re_data[i][j])
-    return recover
 
+    for iii in range(6):
+        FFF = 0
+        recover = []
+        real = ['.'] * 12
+        for j in range(12):
+            if re_data[iii][j] != '.':
+                FFF = 1
+                recover.append(re_data[iii][j])
+        if FFF:
+            for k in range(len(recover)):
+                if real[k] =='.':
+                    real[k] = recover[k]
+        re_data[iii] = real
+    bfs()  # 이거 인덴테이션 주의하가ㅣ.....
 
 def bfs():
+    global cnt
     F = 0
-    global RR,GG,BB,YY,PP, cnt
     for i in range(6):
         for j in range(12):
             if re_data[i][j] in ['R','G','B','Y','P']:
@@ -30,18 +38,18 @@ def bfs():
                 dx = (0,0,-1,1)
                 dy = (-1, 1, 0,0)
                 visit = []
-                que.append((0, 0))
+                que.append((i, j))
                 visit.append((i,j))
                 while que:
                     x,y = que.pop(0)
-                    RR, GG, BB, YY, PP = check(x, y)
-                    for i in range(len(dx)):
-                        nx = x+dx[i]
-                        ny = y+dy[i]
+
+                    for k in range(len(dx)):
+                        nx = x+dx[k]
+                        ny = y+dy[k]
                         if nx<0 or ny<0 or nx>=6 or ny>=12:continue
                         if re_data[nx][ny] != re_data[x][y] or (nx, ny) in visit: continue
 
-                        # RR, GG, BB, YY, PP = check(nx, ny)
+
                         que.append((nx,ny))
                         visit.append((nx,ny))
                 LEN = int(len(visit))
@@ -50,13 +58,13 @@ def bfs():
                     for V in range(len(visit)):
                         VX, VY = visit.pop()
                         re_data[VX][VY] = '.'
+
     if F:
         cnt +=1
-        print(clean())
+        clean()
+
     else:
         return
-`
-
 
 T = int(input())
 for tc in range(1,T+1):
@@ -68,8 +76,9 @@ for tc in range(1,T+1):
 
     for i in range(6):
         print(*re_data[i])
+    print()
     cnt = 0
-    RR, GG, BB, PP, YY = 0, 0, 0, 0, 0
     bfs()
     for i in range(6):
         print(*re_data[i])
+    print(cnt)
