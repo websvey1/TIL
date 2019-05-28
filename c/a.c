151dbbf78cf08d486e1f6676465086f9bc6036d7
이호중 <websvey1@gmail.com>
2019-05-28 17:18:34 +0900

    c_study_01

---
+++ c/a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// int main(void) {
+//     int num;
+//     num = 999999;
+
+//     // printf("hello ");
+//     // printf("Wow! \n");
+//     printf("num is   %d \n", num);
+
+//     return 0;
+// }
+int main(void){
+    double pi;
+    pi = 3.141592654;
+    printf("%.9f", pi);
+}
