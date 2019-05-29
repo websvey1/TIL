f5db01b7e784fab9610c8cb753642fd886296c79
이호중 <websvey1@gmail.com>
2019-05-29 17:40:48 +0900

    study|c|int

---
+++ c/190529.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+// int main(void){
+//     char a = 97;
+//     printf("%c", a);
+// }
+// void main(){
+//     int money; // 변수선언
+//     money = 3000; // 변수에 값 3000 할당
+//     printf("money's value %d", money);
+// }
+// // 호출하기
+// int Sum(int v1, int v2){
+//     int result = v1 + v2;
+//     return result;
+// }
+// void main(){
+//     int a = 2, b = 3, v;
+//     v = Sum(a,b);
+//     printf("sum result is %d", v);
+// }
+// // void와  return
+// int Test(int v1, int v2){  //반황형이 void면 오류...
+//     int result = v1+v2;
+//     return result;
+// }
+// int main(){
+
+// }
+//-1과 최댓값 비교하기
+void main(){
+    char data1 = -1;
+    unsigned char data2 =255;
+    printf("%d, %u, %d, %u", data1, data1, data2, data2);
+}
---
+++ c/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+
+    int result = v1 + v2;
+    return result;
+}
