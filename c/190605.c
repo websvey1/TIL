0c71a423e4ef5b6bb096b0fc3c380b3278e2cd32
이호중 <websvey1@gmail.com>
2019-06-05 17:30:28 +0900

    c_study_arr 2019_06_05

---
+++ c/190605.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// // 배열 만들기
+// int main(){
+//     short student[20];
+//     int i = 0;
+//     for (i; i<20; i++){
+//         printf("%d ",student[i]);
+//     }
+    
+// }
+// // for 문으로 20개의 배열을 0으로 채우기
+// int main(){
+//     short arr[20] , i;
+//     for (i;i<20;i++){
+//         arr[i]=0;
+//     }
+// }
+
+int main(){
+    int a = 'asd';
+    printf("%s", a);
+}
